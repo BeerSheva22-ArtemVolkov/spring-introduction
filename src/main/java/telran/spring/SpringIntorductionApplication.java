e76416193ba4237de5bd795463aabe230335c01a
Art96em <artem-w0lf@yandex.ru>
2023-08-07 15:26:07 +0300

    befaore start security

--- src/main/java/telran/spring/SpringIntorductionApplication.java
+++ src/main/java/telran/spring/SpringIntorductionApplication.java
@@ -3,6 +3,7 @@ package telran.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication // Указывает, что это bean
 public class SpringIntorductionApplication {
 
@@ -11,13 +12,24 @@ public class SpringIntorductionApplication {
 
 		// 1арг - передается объект класса над которым стоят необходимые для построения аппликационного контекста аннотации (@SpringBootApplication)
 
-		// Смысл - построить аппликационную (аннотацию)?
+		// Смысл - построить аппликационный контекст
 		// Сканирует все package-ы, начниая с того, где находится этот класс (в данном случае - telran.spring)
+		// Ищет аннотации:
+		// - @RestController (с ним работает bin servlet)
+		// - @ControllerAdvice
+		// - @Service
+		// - 
 		SpringApplication.run(SpringIntorductionApplication.class, args);
 
 		// с объектами работаем "мы", а с bean-ми Spring
-		// bean - это объект управляемый Spring-ом, находящийся в операционном контексте
+		// bean - это объект управляемый Spring-ом, находящийся в аппликационном контексте
 
 	}
 
 }
+
+// spring security - интеграция с протоколами
+
+// bin-authentication обращается к bin-detail-service с данными юзера
+// bin-authorization (принимает правила) ему передает bin-auth 
+// bin-auth обращается к authorization-rules 
